d750061bdd9e1ce4bf811ae304aff89fca3b7517
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-05-22 12:46:48 +0000

    [Update]投稿機能を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -8,13 +9,25 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
   
   def create
     @book=Book.new(book_params)
     @book.save
     redirect_to book_path(@book)
-    
+  end
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources:books
   root:to => 'homes#top'
-  post 'books'=> 'books#create'
+  #post 'books'=> 'books#create'
+  #get 'books/:id'=>'books#show'
+  #get 'books/:id/edit'=>'books#edit'
+  #patch 'books/:id'=>'books#update',as:'update_book'
+  #delete 'books/:id'=>'books#destroy',as:'destroy_books'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
