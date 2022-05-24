73081eff97e9130f993246854b64241eca8fbe30
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-05-24 16:26:21 +0000

    [Update]微修正１

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,29 +5,42 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
   
   def create
-    @book=Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render:index
+    end
   end
   
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      
+      render:edit
+      
+    end
   end
   
   def destroy
-    book=Book.find(params[:id])
+    book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully deleted."
+    redirect_to books_path
+    
   end
   
   private
